6cbf89d137f81db39420af1ea37f48d9479e17b9
Esteban Padilla <ep@estebanpadilla.com>
2017-05-14 11:26:52 -0600

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NoteComponent } from './components/note/note.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { OptionsComponent } from './components/options/options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    NoteComponent,
+    NotesComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
